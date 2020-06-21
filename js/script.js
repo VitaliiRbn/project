4c9d617e9fa2873726ae473b4c74173affb8537c
Vitalii <Vitaliy.Rbn@gmail.com>
2020-06-21 23:04:07 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+//alert ('Hello!');
+
+//const result = confirm("Are you here ?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18 ?", "18");
+//console.log(typeof(answer));
+
+//const answers = [];
+//answers[0] = prompt('Как Ваше имя?', '');
+//answers[1] = prompt('Как Ваше фамилия?', '');
+//answers[2] = prompt('Сколько Вам лет?', '');
+
+//document.write(answers);
+
+
+///const category = 'toys';
+//console.log('http://someurl.com/'+ category);
+
+//const user = "Ivan";
+//alert(`Hello, ${user}`);
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
