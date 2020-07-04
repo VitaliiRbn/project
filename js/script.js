43549c8c066c4b99be1bcf6216449652013611b0
Vitalii <Vitaliy.Rbn@gmail.com>
2020-07-04 21:50:45 +0300

    Begin to write code

--- js/script.js
+++ js/script.js
@@ -1,40 +1,19 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
+const PersonalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
 
-//alert ('Hello!');
+const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его', '');
 
-//const result = confirm("Are you here ?");
-//console.log(result);
+      PersonalMovieDB.movies[a] = b;
+      PersonalMovieDB.movies[c] = d;
 
-//const answer = prompt("Вам есть 18 ?", "18");
-//console.log(typeof(answer));
-
-//const answers = [];
-//answers[0] = prompt('Как Ваше имя?', '');
-//answers[1] = prompt('Как Ваше фамилия?', '');
-//answers[2] = prompt('Сколько Вам лет?', '');
-
-//document.write(answers);
-
-
-///const category = 'toys';
-//console.log('http://someurl.com/'+ category);
-
-//const user = "Ivan";
-//alert(`Hello, ${user}`);
-
-//let incr = 10,
-  //  decr = 10;
-//incr++;
-//decr--;
-//console.log(incr);
-//console.log(decr);
-
-console.log(2*4 === '8');
-
-const isChecked = true,
-    isClose = false;
-    console.log(isChecked || !isClose);
-
-
-
-
+      console.log(PersonalMovieDB);
